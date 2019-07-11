6cddedcb3c8cdf81a1b282fee7dcfd256b8f1082
Salman Nazir <salman.nazir@coyoapp.com>
2019-07-11 08:00:51 +0200

    New Android Project Project Structure Manifest Buid.gradle Linear Layout Relative layout Android Views in XML

---
+++ app/src/main/java/com/theta/androidkotlin/LinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.theta.androidkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
